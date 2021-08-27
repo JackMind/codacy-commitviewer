eb9580d909482a27cc48cd3c9a58312f5aea35cd
Rúben Santos <rubensantos@Rubens-MacBook-Pro.local>
2021-08-27 17:01:19 +0100

    Added swagger and rest controller constrain validations

--- src/main/java/com/codacy/commitviewer/api/controllers/CommitController.java
+++ src/main/java/com/codacy/commitviewer/api/controllers/CommitController.java
@@ -5,12 +5,20 @@ import com.codacy.commitviewer.api.mapper.CommitMapper;
 import com.codacy.commitviewer.domain.commitviewer.service.CommitViewerAggregator;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -23,6 +31,7 @@ import java.util.Optional;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@Validated
 public class CommitController {
 
     private final CommitViewerAggregator commitViewerAggregator;
@@ -30,12 +39,22 @@ public class CommitController {
 
     @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
     List<CommitDto> getCommitsLocal(@RequestParam("url") @NotNull final String url,
-                                    @RequestParam("per_page") @NotNull @Max(100) final Integer per_page,
+                                    @RequestParam("per_page") @Valid @NotNull @Max(100) final Integer per_page,
                                     @RequestParam(value = "page", required = false) @Min(1) final Integer page,
                                     @RequestParam(value = "force_local", required = false) final Boolean forceLocal){
         log.trace("url: {} per_page:{} page:{} forceLocal:{}", url, per_page, page, forceLocal);
         boolean local = !Objects.isNull(forceLocal) && forceLocal;
         int pageInt = Optional.ofNullable(page).orElse(1);
         return commitMapper.from( commitViewerAggregator.getCommits(url, per_page, pageInt, local) );
+    }
+
+    @EnableWebMvc
+    @Configuration
+    @ComponentScan("com.codacy.commitviewer")
+    public static class ClientWebConfigJava implements WebMvcConfigurer {
+        @Bean
+        public MethodValidationPostProcessor methodValidationPostProcessor() {
+            return new MethodValidationPostProcessor();
+        }
     }
 }
